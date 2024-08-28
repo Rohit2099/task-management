2af093a0a9387b2d43535f69f7f12f47eec88e60
I539173 <rohit.ravichandran@sap.com>
2024-08-28 19:46:30 +0530

    style nav bar

--- src/components/Elements/NavBar.jsx
+++ src/components/Elements/NavBar.jsx
@@ -12,13 +12,22 @@ export default function NavBar() {
     const title = pathToTitleMap[location.pathname] ?? "Error";
     const isHomePage = location.pathname === "" || location.pathname === "/";
     return (
-        <div>
-            {!isHomePage && (
-                <button type="text" onClick={() => navigate(-1)}>
-                    Back button
-                </button>
-            )}
-            <span>{title}</span>
-        </div>
+        <nav className="border-blue-500 bg-blue-800">
+            <div className="max-w-full p-3">
+                <span className="flex items-center space-x-4 pl-1">
+                    {!isHomePage && (
+                        <img
+                            src="../../../public/left-chevron.png"
+                            className="h-6 hover:cursor-pointer"
+                            alt="Back button"
+                            onClick={() => navigate(-1)}
+                        />
+                    )}
+                    <span className="text-2xl font-semibold text-white">
+                        {title}
+                    </span>
+                </span>
+            </div>
+        </nav>
     );
 }
