fe14d0f9d9b151dd0cfd4cda23ae73d3394ba7ad
I539173 <rohit.ravichandran@sap.com>
2024-08-30 16:32:22 +0530

    spelling

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -7,7 +7,7 @@ export default function ErrorPage() {
             <div className="mx-auto max-w-full py-16 px-6 text-center">
                 <h1 className="mb-4 font-extrabold text-9xl">404</h1>
                 <p className="mb-4 text-lg font-light text-gray-500">
-                    Are you sure you're on the write URL? Check again.
+                    Are you sure you're on the right URL? Check again.
                 </p>
                 <button
                     onClick={() => navigate("/")}
